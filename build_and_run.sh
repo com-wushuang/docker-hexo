7e9285afd420fe7b0bda583eff3b6b28e36cfa86
chengjun <mybestchen@163.com>
2020-11-26 17:11:26 +0800

    hexo框架，dockerfile制作镜像，build_and_run.sh启动脚本

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,11 @@
+# 把hexo框架clone到容器中
+git clone https://github.com/com-wushuang/docker-hexo.git
+
 # 将文章clone到容器中
 git clone https://github.com/com-wushuang/blog.git
-mv blog/* source/_posts
+mv blog/* docker-hexo/source/_posts
+
+cd docker-hexo/
 npm install
 hexo generate
 hexo server -p 80
